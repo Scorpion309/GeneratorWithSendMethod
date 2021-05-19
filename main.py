e08bd3c8d16831791c7d1d26039a06e7517af168
Scorpion309 <Tustin@tut.by>
2021-05-19 21:14:14 +0300

    add method .throw()

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 def generator():
     x = 0
     while True:
-        flag = yield x
-        if flag is not None:
-            x += 1
+        try:
+            flag = yield x
+            if flag is not None:
+                x += 1
+                yield x
+            else:
+                x += 2
+        except ValueError:
+            x = 0
             yield x
-        else:
-            x += 2
+            print('Сброс генератора')
+
 
 
 if __name__ == '__main__':
     gen = generator()
+    print(next(gen))
+    print(next(gen))
+    print(next(gen))
+    gen.send(False)
+    print(next(gen))
+    print(next(gen))
+    gen.send(True)
+    print(next(gen))
+    gen.throw(ValueError, "To zero!")
     print(next(gen))
     print(next(gen))
     print(next(gen))
